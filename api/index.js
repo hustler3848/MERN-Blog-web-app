77e53a95e4269dc2f49b301bbf7099c1d996a742
OtherAccountName <lamichhane3848@gmail.com>
2025-08-29 20:37:12 +0545

    feat: implement sign-out functionality and improve header component structure

--- api/index.js
+++ api/index.js
@@ -46,3 +46,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -17,11 +17,31 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { toogleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
+
 function Header() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const path = useLocation().pathname;
-  const { currentUserOfBloggingApp } = useSelector((state) => state.userOfBloggingApp);
-  const {theme} = useSelector((state)=> state.theme)
+  const { currentUserOfBloggingApp } = useSelector(
+    (state) => state.userOfBloggingApp
+  );
+  const { theme } = useSelector((state) => state.theme);
+
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Navbar className="border-b-2 fixed top-0 mb-10 z-30 w-full shadow-md dark:bg-slate-950">
       <Link
@@ -47,7 +67,7 @@ function Header() {
         <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        
+
         <Navbar.Link active={path === "/dashboard"} as={"div"}>
           <Link to="/dashboard">Dashboard</Link>
         </Navbar.Link>
@@ -59,28 +79,47 @@ function Header() {
         </Navbar.Link>
       </NavbarCollapse>
       <div className="flex flex-row gap-2 mf:order-2">
-        <Button className="hidden sm:inline" color="gray" onClick={()=> dispatch(toogleTheme())} >
-          {theme === 'light' ? (<FaMoon />): (<FaSun/>)}
+        <Button
+          className="hidden sm:inline"
+          color="gray"
+          onClick={() => dispatch(toogleTheme())}
+        >
+          {theme === "light" ? <FaMoon /> : <FaSun />}
         </Button>
         {currentUserOfBloggingApp ? (
           <Dropdown
             arrowIcon="false"
             inline
-            label={<Avatar alt="user" img={currentUserOfBloggingApp.profilePic} className="object-cover" rounded size='sm'/>}
+            label={
+              <Avatar
+                alt="user"
+                img={currentUserOfBloggingApp.profilePic}
+                className="object-cover"
+                rounded
+                size="sm"
+              />
+            }
           >
             <Dropdown.Header>
-              <span className="block text-sm">@{currentUserOfBloggingApp.username}</span>
-              <span className="block text-md">{currentUserOfBloggingApp.email}</span>
-              <Link to='/dashboard?tab=profile'>
-              <DropdownItem>Profile</DropdownItem>
+              <span className="block text-sm">
+                @{currentUserOfBloggingApp.username}
+              </span>
+              <span className="block text-md">
+                {currentUserOfBloggingApp.email}
+              </span>
+              <Link to="/dashboard?tab=profile">
+                <DropdownItem>Profile</DropdownItem>
               </Link>
               <DropdownDivider />
-              <DropdownItem>Sign Out</DropdownItem>
+
+              <DropdownItem onClick={handleSignOut}>Sign Out</DropdownItem>
             </Dropdown.Header>
           </Dropdown>
         ) : (
           <Link to="/login">
-            <Button gradientDuoTone="purpleToBlue" outline>Login</Button>
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Login
+            </Button>
           </Link>
         )}
 
