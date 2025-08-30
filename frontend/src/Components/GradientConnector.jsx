e732c988b79f50f509f5d3d8c3dce3e983021a53
OtherAccountName <lamichhane3848@gmail.com>
2025-08-30 06:10:12 +0545

    refactor: adjust layout and height of GradientConnector component

--- frontend/src/Components/GradientConnector.jsx
+++ frontend/src/Components/GradientConnector.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function GradientConnector() {
   return (
-    <div className="relative w-screen overflow-hidden">
-        <div className="h-[50vh] bg-gradient-to-b from-[#1F1327] to-[#171717]"></div>
+    <div className="overflow-hidden">
+        <div className="h-[20vh] bg-gradient-to-b from-[#1F1327] to-[#171717]"></div>
       {/* <div className="absolute -inset-1 bg-gradient-to-r from-sky-600 z-0 to-teal-500 rounded-lg blur opacity-35 max-w-screen"></div> */}
     </div>
   );
